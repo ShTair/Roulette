e2e20c9dddbe1e806b1fa62183c86a657a01ec36
ShTair <git@vsh.jp>
2025-01-04 22:06:00 +0900

    設定ウィンドウとの通信を変更

--- Roulette/Program.cs
+++ Roulette/Program.cs
@@ -1,11 +1,13 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Roulette;
+using Roulette.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSingleton<UpdateConfigEventSource>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ Roulette/Services/UpdateConfigEventSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.JSInterop;
+
+namespace Roulette.Services;
+
+public class UpdateConfigEventSource
+{
+    private static UpdateConfigEventSource? s_obj;
+
+    public event Action<string>? OnUpdateConfig;
+
+    public UpdateConfigEventSource()
+    {
+        s_obj = this;
+    }
+
+    [JSInvokable]
+    public static Task UpdateConfigAsync(string data)
+    {
+        s_obj?.OnUpdateConfig?.Invoke(data);
+        return Task.CompletedTask;
+    }
+}
