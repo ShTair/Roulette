6ed3ef7cd2aaf90733992256510f11fcf2451a20
ShTair <git@vsh.jp>
2025-01-05 16:21:43 +0900

    圧縮完成

--- Roulette/Models/Serializer.cs
+++ Roulette/Models/Serializer.cs
@@ -1,5 +1,5 @@
 ﻿using MessagePack;
-using System.Buffers;
+using NeoSmart.Utils;
 using System.IO.Compression;
 
 namespace Roulette.Models;
@@ -9,28 +9,20 @@ public static class Serializer
     public static string Serialize<T>(T obj)
     {
         using var memoryStream = new MemoryStream();
-        using var deflateStream = new DeflateStream(memoryStream, CompressionLevel.Fastest);
+        using var deflateStream = new DeflateStream(memoryStream, CompressionLevel.Optimal);
         deflateStream.Write(MessagePackSerializer.Serialize(obj));
         deflateStream.Flush();
-        return Convert.ToBase64String(memoryStream.GetBuffer().AsSpan()[..(int)memoryStream.Length]);
+
+        return UrlBase64.Encode(memoryStream.GetBuffer().AsSpan()[..(int)memoryStream.Length]);
     }
 
     public static T Deserialize<T>(string s)
     {
-        using var memoryStream = new MemoryStream(Convert.FromBase64String(s));
-        using var deflateStream = new DeflateStream(memoryStream, CompressionLevel.Fastest);
+        using var memoryStream = new MemoryStream(UrlBase64.Decode(s));
+        using var deflateStream = new DeflateStream(memoryStream, CompressionLevel.Optimal);
         using var dstMemoryStream = new MemoryStream();
-
         deflateStream.CopyTo(dstMemoryStream);
 
-        var buffer = ArrayPool<byte>.Shared.Rent((int)deflateStream.Length);
-        try
-        {
-            return MessagePackSerializer.Deserialize<T>(dstMemoryStream.GetBuffer().AsMemory()[..(int)dstMemoryStream.Length]);
-        }
-        finally
-        {
-            ArrayPool<byte>.Shared.Return(buffer);
-        }
+        return MessagePackSerializer.Deserialize<T>(dstMemoryStream.GetBuffer().AsMemory()[..(int)dstMemoryStream.Length]);
     }
 }
