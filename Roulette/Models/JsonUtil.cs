0c7a5ef41cc6cbc363449b82cee8acf3d81efc8d
ShTair <ShTair@users.noreply.github.com>
2025-07-29 14:57:50 +0900

    jsonの設定を変更

--- Roulette/Models/JsonUtil.cs
+++ Roulette/Models/JsonUtil.cs
@@ -1,8 +1,12 @@
-namespace Roulette.Models;
-
+using System.Text.Encodings.Web;
 using System.Text.Json;
+
+namespace Roulette.Models;
 
 public static class JsonUtil
 {
-    public static readonly JsonSerializerOptions WebOptions = new(JsonSerializerDefaults.Web);
+    public static readonly JsonSerializerOptions WebOptions = new(JsonSerializerDefaults.Web)
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    };
 }
